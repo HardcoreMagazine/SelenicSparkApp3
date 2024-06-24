8abeba457378df8d6173fc29c51f0e6c90479aa5
HardcoreMagazine <hardcoremagazine@users.noreply.github.com>
2024-06-24 16:29:42 +0300

    begin work on AuthService [checkpoint]

---
+++ AuthService/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+using AuthService.Models;
+using AuthService.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(ILogger<AuthController> logger, AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public void Get()
+        {
+            return;
+        }
+    }
+}
