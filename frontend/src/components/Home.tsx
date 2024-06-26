433c386673e5dd65e0b2a9cd25f28383ffac7c4a
HardcoreMagazine <hardcoremagazine@users.noreply.github.com>
2024-06-26 17:51:21 +0300

    better-looking homepage; links now have new color & color hover change

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -3,26 +3,22 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <>
-      <nav className="dark:bg-gray-600">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <nav className="bg-gradient-to-b from-slate-950 to-indigo-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between p-3">
           <Link to="/">
             <div className="inline-flex">
               <img src="./public/logo.png" className="h-8" alt="SelenicSpark3 Logo" />
               &nbsp;
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SelenicSpark3</span>
+              <span className="self-center text-2xl font-semibold whitespace-nowrap text-">SelenicSpark3</span>
             </div>
           </Link>
-          <div className="hidden w-full md:block md:w-auto">
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <div>
+            <ul className="font-medium flex space-x-10">
               <li>
                 <Link to="/">Home</Link>
               </li>
               <li>
-                <Link to="/posts">
-                  <div className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                    Posts
-                  </div>
-                </Link>
+                <Link to="/posts">Posts</Link>
               </li>
             </ul>
           </div>
@@ -44,11 +40,33 @@ function Home() {
               License type: Apache 2.0
             </li>
             <li className="mt-3">
-              <a href='https://github.com/HardcoreMagazine/SelenicSparkApp3' target='_blank'>Project Github page</a>
+              <a href='https://github.com/HardcoreMagazine/SelenicSparkApp3' target='_blank'>
+                Project Github page
+              </a>
             </li>
           </ul>
         </div>
       </main>
+      <footer>
+        <div className="w-full flex flex-wrap items-center p-4 bg-gradient-to-t from-slate-950 to-indigo-900">
+          <ul className="inline-flex space-x-10">
+            <li>
+              <Link to="/">
+                  <span className="self-center whitespace-nowrap">&copy; 2024 SelenicSpark3</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/">About</Link>
+            </li>
+            <li>
+              <Link to="/privacy">Privacy</Link>
+            </li>
+          </ul>
+        </div>
+      </footer>
     </>
   );
 }
