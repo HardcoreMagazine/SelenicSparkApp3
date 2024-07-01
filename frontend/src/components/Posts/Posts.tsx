c8869e4a125d075155e4e02ac0b953a53fedce02
HardcoreMagazine <hardcoremagazine@users.noreply.github.com>
2024-07-01 15:10:57 +0300

    Posts page rework, sort of better posts view; added few more placeholders for posts.

--- frontend/src/components/Posts/Posts.tsx
+++ frontend/src/components/Posts/Posts.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { IPost } from './IPost'
 import { format, parseISO } from 'date-fns'
+import { Link } from 'react-router-dom'
 
 function Posts() {
   const [posts, setPosts] = useState<IPost[]>();
@@ -12,11 +13,22 @@ function Posts() {
   const mainPageContent = posts === undefined ? (
     <h1>Loading, please wait</h1>
   ) : (
-    <ul>
+    <ul className="space-y-6">
       {posts.length > 0 && (
         posts.map(post => 
-          <li key={post.id} className="border border-blue-400">
-            {post.dateCreated} {post.author} {post.title}
+          <li key={post.id} className="border-2 border-indigo-800 py-6 mx-12 bg-opacity-10 bg-slate-500 rounded-md">
+            <Link to={"/post/"+post.id} className="break-all flex mx-10">
+              {post.title}
+            </Link>
+            <span className="text-xs font-light flex justify-between mx-32 mt-6">
+              <p>
+                {post.dateCreated} @ by <span className="text-blue-400">{post.author}</span>
+              </p>              
+              <p>
+                &lt;user reactions placeholder&gt; 
+                &lt;user comments placeholder&gt;
+              </p>
+            </span>
           </li>
         )
       )}
@@ -28,14 +40,14 @@ function Posts() {
 
   return (
     <>
-      <div>
+      <div className="mt-12">
         <button className="">Create new placeholder</button>
       </div>
-      <div>
+      <div className="my-7">
         {mainPageContent}
       </div>
-      <div>
-        &lt; pagination placeholder &gt;
+      <div className="mb-12">
+        &lt;pagination placeholder&gt;
       </div>
     </>
   );
