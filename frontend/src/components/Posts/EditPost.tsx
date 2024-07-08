f0f8fd424dc748ea8270739ebaaf255cabbd363f
HardcoreMagazine <hardcoremagazine@users.noreply.github.com>
2024-07-08 20:48:58 +0300

    savepoint

--- frontend/src/components/Posts/EditPost.tsx
+++ frontend/src/components/Posts/EditPost.tsx
@@ -1,7 +1,100 @@
+import { useEffect, useRef, useState } from "react"
+import { IPost } from './IPost'
+import { useParams } from "react-router-dom";
+import { formatDtString } from "../Shared/FuncFormatDtString";
+
 function EditPost() {
-  return (
-    <p>Hello world!</p>
+  const { id } = useParams();
+
+  const [post, setPost] = useState<IPost>();
+
+  useEffect(() => {
+    populateData();
+  }, []);
+
+
+  // this will allow to auto-resize textarea HTML element
+  // kudos to github.com/codewgi
+  const textRef = useRef("");
+  const [txt, setTxt] = useState<string>();
+  // needed by law, otherwise browser renders read-only input
+  const txtChangeHandler = (e) => {
+    setTxt(e.target.value);
+  };
+
+  useEffect(() => {
+    textRef.current.style.height = "auto";
+    textRef.current.style.height = textRef.current.scrollHeight + "px";
+  }, [txt]);
+
+  const createHandler = async (e) => {
+    e.preventDefault();
+
+    // unsafe but should work in absolute majority of cases
+    const formData: IPost = {} as IPost;
+    // dirty
+    // len-1 because <button> also counts as element as gets written in JSON    
+    for (let i = 0; i < e.target.length - 1; i++) {
+      formData[e.target[i].name] = e.target[i].value;
+    }
+    const jsonData = JSON.stringify(formData);
+
+    const res = await fetch("https://localhost:46801/post", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: jsonData
+    });
+
+    const data = await res.json();
+    afterCreateHandler(data);
+  }
+
+  const afterCreateHandler = (responseCode: number) => {
+    if (responseCode > 0) {
+      window.location.replace(`/post/${responseCode}`);
+    }
+  }
+
+  const mainPageContent = post === undefined ? (
+    <h1>Loading, please wait</h1>
+  ) : (
+    (setTxt(post.text), true) && (
+    <>
+      <div className="space-y-6">
+        <h1 className="text-4xl mt-8">
+          Post editing mode
+        </h1>
+        <form method="post" onSubmit={createHandler}>
+          <div className="grid mx-16 mb-4 space-y-3">
+            <input name="id" value={post.id} hidden readOnly />
+            <input name="author" value={post.author} hidden readOnly />
+            <input name="dateCreated" value={post.dateCreated} hidden readOnly />
+            <input name="title" value={post.title} readOnly placeholder="Title" maxLength="256" minLength="15" autoComplete="off" className="p-2 rounded-lg break-all border bg-stone-900 border-stone-500" />
+            <textarea name="text" placeholder="Text (optional)" autoComplete="off" maxLength="24000" value={txt} onChange={txtChangeHandler} ref={textRef} className="flex flex-col p-2 rounded-lg border bg-stone-900 border-stone-500" />
+          </div>
+          <button type="submit" className="py-1 px-32 mb-8 rounded-lg text-lg border border-indigo-400 bg-rose-800 hover:bg-rose-700 hover:text-indigo-300 select-none">
+            PUBLISH
+          </button>
+        </form>
+      </div>
+    </>
+    )
   );
+
+  return (
+    <>
+      {mainPageContent}
+    </>
+  );
+
+  async function populateData() {
+    const res = await fetch(`https://localhost:46801/post/${id}`);
+    const data: IPost = await res.json();
+    data.dateCreated = formatDtString(data.dateCreated);
+    setPost(data);
+  }
 }
 
 export default EditPost;
